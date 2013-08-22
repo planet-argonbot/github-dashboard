7b38e2f77573ceb719c9c65d93b92eca08ddd06b
James Anderson <me@jamesaanderson.com>
2013-08-22 11:13:06 -0700

    Add sparklines

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,18 @@
 $(document).on('page:fetch', function() { NProgress.start(); });
 $(document).on('page:change', function() { NProgress.done(); });
 $(document).on('page:restore', function() { NProgress.remove(); });
+
+$(document).ready(function() {
+  $('.sparkline').sparkline('html', {
+    type: 'line',
+    width: '136',
+    height: '28',
+    lineColor: '#000000',
+    fillColor: null,
+    spotColor: '#000000',
+    minSpotColor: '#000000',
+    maxSpotColor: '#000000',
+    highlightSpotColor: '#000000',
+    highlightLineColor: '#000000'
+  });
+});
--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -9,16 +9,23 @@ class Contributor
   end
 
   def last_contributed_at
-    Date.parse(
-      commits.first.commit.author.date
-    )
+    Date.parse(commits.first.commit.author.date)
+  end
+
+  def contributions_for_week
+    commits.each_with_object(Array.new(7, 0)) do |commit, result|
+      date = Date.parse(commit.commit.author.date)
+      if date >= 1.week.ago.to_date
+        result[date.cwday - 1] += 1
+      end
+    end
   end
 
   private
 
   def commits
     Rails.cache.fetch("contributor:commits:#{user}:#{repo}", expires_in: 10.minutes) do
-      client.commits(repo, 'master', { author: user })
+      client.commits(repo, 'master', author: user)
     end
   end
 end
