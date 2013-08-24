fc812a000f1a5e568c6fede901e42a6baae514fe
James Anderson <me@jamesaanderson.com>
2013-08-24 06:04:32 -0700

    Fix spec after updating Contributor#contributions_for_week

--- spec/models/contributor_spec.rb
+++ spec/models/contributor_spec.rb
@@ -27,7 +27,7 @@ describe Contributor do
     it "returns a contributor's contributions for the current week" do
       expect(contributor.contributions_for_week).to be_a(Array)
       expect(contributor.contributions_for_week.count).to eq(7)
-      expect(contributor.contributions_for_week).to eq([1, 3, 8, 1, 0, 1, 0])
+      expect(contributor.contributions_for_week).to eq([1, 3, 1, 0, 0, 0, 0])
     end
   end
 end
